e336183d9b82a731c0ff5f1a01f04d641d8605ac
Brian Taylor Vann <brian.t.vann@gmail.com>
2023-01-31 20:17:55 -0500

    change app to file-server (#1)

--- v0.1/build_container_files.sh
+++ v0.1/build_container_files.sh
@@ -7,7 +7,7 @@
 curr_dir=`dirname $0`
 
 config_path=$curr_dir/config
-file_server_path=$curr_dir/app
+file_server_path=$curr_dir/file-server
 podmanfile_path=$curr_dir/resources/file-server.podmanfile
 podman_compose_path=$curr_dir/resources/podman-compose.yml.template
 container_path=$curr_dir/container/Cargo.toml
