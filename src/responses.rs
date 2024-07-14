8b0a9dfe944ebfbc8f466f9611f969f68fc9b793
Taylor Vann <brian.t.vann@gmail.com>
2024-07-14 12:41:51 -0700

    flatten nested logic (#23)

--- src/responses.rs
+++ src/responses.rs
@@ -22,65 +22,72 @@ pub fn get_pathbuff_from_request(
     req: &Request<IncomingBody>,
 ) -> (Option<path::PathBuf>, Option<path::PathBuf>, Option<String>) {
     // get path and strip initial forward slash
-    let uri_path = req.uri().path();
-    let mut path = match uri_path.strip_prefix(FWD_SLASH) {
-        Some(p) => dir.join(p),
-        _ => dir.join(uri_path),
+    let source_dir = match path::absolute(dir) {
+        Ok(sdf) => sdf,
+        _ => return (None, None, None),
     };
 
+    // asbolute the source_dir
     // flatten path (ie ../../)
-    if let Some(file_name_str) = path.file_name() {
-        path = path::PathBuf::from(file_name_str);
+    let uri_path = req.uri().path();
+    let mut target_path = match uri_path.strip_prefix(FWD_SLASH) {
+        Some(p) => source_dir.join(p),
+        _ => source_dir.join(uri_path),
+    };
+    // look for index.html if directory
+    if target_path.is_dir() {
+        target_path = target_path.join(INDEX);
     }
+    target_path = match path::absolute(target_path) {
+        Ok(sdf) => sdf,
+        _ => return (None, None, None),
+    };
 
     // confirm path resides in directory
-    if !path.starts_with(dir) {
-        // and exists?
+    if !target_path.starts_with(source_dir) {
         return (None, None, None);
     }
 
-    // look for index.html if directory
-    if path.is_dir() {
-        path = path.join(INDEX);
-    }
-
-    // check if file exists
-    if let Ok(exists) = path.try_exists() {
+    // check if encoded file exists
+    if let Ok(exists) = target_path.try_exists() {
         if !exists {
             return (None, None, None);
         }
     }
 
-    if let (Some(encoding_path), Some(trimmed)) =
-        get_enc_path(&path, req.headers().get(ACCEPT_ENCODING))
-    {
-        return (Some(path), Some(encoding_path), Some(trimmed));
-    }
+    let (encoding_path, trimmed) = get_enc_path(&target_path, req.headers().get(ACCEPT_ENCODING));
 
     // otherwise serve file
-    (Some(path), None, None)
+    (Some(target_path), encoding_path, trimmed)
 }
 
+// target path must be absolute for this to work
 fn get_enc_path(
-    path: &path::PathBuf,
+    target_path: &path::PathBuf,
     encoding_header: Option<&HeaderValue>,
 ) -> (Option<path::PathBuf>, Option<String>) {
-    if let Some(encoding_value) = encoding_header {
-        if let Ok(encoding_str) = encoding_value.to_str() {
-            for encoding in encoding_str.split(",") {
-                let trimmed = encoding.trim();
-                if let Some(ext) = get_ext(trimmed) {
-                    let encoding_path = path.join(ext);
+    let header = match encoding_header {
+        Some(enc) => enc,
+        _ => return (None, None),
+    };
 
-                    if let Ok(exists) = encoding_path.try_exists() {
-                        if exists {
-                            return (Some(encoding_path), Some(trimmed.to_string()));
-                        }
-                    }
+    let encoding_str = match header.to_str() {
+        Ok(s) => s,
+        _ => return (None, None)
+    };
+
+    for encoding in encoding_str.split(",") {
+        let trimmed = encoding.trim();
+        if let Some(ext) = get_ext(trimmed) {
+            let encoding_path = target_path.join(ext);
+
+            if let Ok(exists) = encoding_path.try_exists() {
+                if exists {
+                    return (Some(encoding_path), Some(trimmed.to_string()));
                 }
             }
         }
-    };
+    }
 
     (None, None)
 }
@@ -96,24 +103,23 @@ fn get_ext(encoding: &str) -> Option<&str> {
 }
 
 pub async fn build_response(
-    req_path: path::PathBuf,
+    req_path: Option<path::PathBuf>,
     encoding_path: Option<path::PathBuf>,
     encoding: Option<String>,
 ) -> Result<BoxedResponse, hyper::http::Error> {
+    let rpath = match req_path {
+        Some(p) => p,
+        _ => return create_error_response(&StatusCode::NOT_FOUND, "404 not found"),
+    };
+    
+    let content_type = get_content_type(&rpath);
     let (path, enc) = match (encoding_path, encoding) {
         (Some(enc_path), Some(enc)) => (enc_path, Some(enc)),
-        _ => (req_path.clone(), None),
+        _ => (rpath.clone(), None),
     };
 
     match File::open(&path).await {
         Ok(file) => {
-            let content_type = get_content_type(&req_path);
-
-            // https://github.com/hyperium/hyper/blob/master/examples/send_file.rs
-            let reader_stream = ReaderStream::new(file);
-            let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
-            let boxed_body = stream_body.boxed();
-
             let mut builder = Response::builder()
                 .status(StatusCode::OK)
                 .header(CONTENT_TYPE, content_type);
@@ -121,6 +127,11 @@ pub async fn build_response(
             if let Some(enc_type) = enc {
                 builder = builder.header(CONTENT_ENCODING, enc_type);
             }
+
+            // https://github.com/hyperium/hyper/blob/master/examples/send_file.rs
+            let reader_stream = ReaderStream::new(file);
+            let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
+            let boxed_body = stream_body.boxed();
 
             builder.body(boxed_body)
         }
--- src/service.rs
+++ src/service.rs
@@ -1,7 +1,6 @@
 use hyper::body::Incoming as IncomingBody;
 use hyper::http::Request;
 use hyper::service::Service;
-use hyper::StatusCode;
 use std::future::Future;
 use std::path;
 use std::pin::Pin;
@@ -18,13 +17,8 @@ impl Service<Request<IncomingBody>> for Svc {
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn call(&self, req: Request<IncomingBody>) -> Self::Future {
-        match responses::get_pathbuff_from_request(&self.directory, &req) {
-            (Some(path), encoded_path, encoding) => {
-                Box::pin(async { responses::build_response(path, encoded_path, encoding).await })
-            }
-            _ => Box::pin(async {
-                responses::create_error_response(&StatusCode::NOT_FOUND, "404 not found")
-            }),
-        }
+        let (path, encoded_path, encoding) = responses::get_pathbuff_from_request(&self.directory, &req);
+
+        Box::pin(async { responses::build_response(path, encoded_path, encoding).await })
     }
 }
