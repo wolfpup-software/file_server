cebf88c17898b28b6d8c479e60d3db47c14763e8
Taylor Vann <brian.t.vann@gmail.com>
2025-03-02 21:24:49 -0800

    ranges build

--- responses/src/range_response.rs
+++ responses/src/range_response.rs
@@ -26,28 +26,32 @@ use crate::type_flyweight::BoxedResponse;
 // multi range requests require an entire different strategy
 // Range: <unit>=<range-start>-<range-end>, …, <range-startN>-<range-endN>
 
+pub const RANGE_NOT_SATISFIABLE_416: &str = "416 range not satisfiable";
+
 pub async fn build_range_response(
     req: &Request<IncomingBody>,
     directory: &PathBuf,
     content_encodings: &Option<Vec<String>>,
 ) -> Option<Result<BoxedResponse, hyper::http::Error>> {
     // bail immediately if no range header
+    //
     let range_header = match get_range_header(req) {
         Some(rh) => rh,
         _ => return None,
     };
 
+    // needs special header
+    // no body
+    //
     let ranges = match get_ranges(&range_header) {
         Some(rngs) => rngs,
         _ => return None,
     };
 
-    //
-
     // parse header with a [(None, Option<usize>), ...]
     // if none return 416
     // then interpret it later
-
+    //
     let filepath = match get_path_from_request_url(&req, &directory).await {
         Some(fp) => fp,
         _ => {
@@ -58,44 +62,23 @@ pub async fn build_range_response(
         }
     };
 
-    // encodings
-    //
-    // if encoded path is available?
-    //      then serve encoded path with range
-
-    // get range sub function
-    let metadata = match fs::metadata(&filepath).await {
-        Ok(m) => m,
-        _ => return None,
-    };
-
-    let size = metadata.len() as usize;
-
-    if 0 == ranges.len() {
-        return None;
-    }
-
-    // then do serve encoded files or files
-    let file_to_read = match File::open(&filepath).await {
-        Ok(f) => f,
-        _ => return None,
-    };
-
     let content_type = get_content_type(&filepath);
     let encodings = get_encodings(&req, &content_encodings);
 
-    // // single range
-    // if 1 == ranges.len() {
-    //     // build response
-    //     return build_single_range_response(
-    //         file_to_read,
-    //         metadata,
-    //         path_details,
-    //         ranges,
-    //         content_type,
-    //     )
-    //     .await;
-    // }
+    // single range
+    if 1 == ranges.len() {
+        if let Some(res) = build_range_responses(
+            &filepath,
+            &content_type,
+            StatusCode::PARTIAL_CONTENT,
+            &encodings,
+            ranges,
+        )
+        .await
+        {
+            return Some(res);
+        }
+    }
 
     Some(build_last_resort_response(
         StatusCode::NOT_FOUND,
@@ -114,120 +97,6 @@ fn get_range_header(req: &Request<IncomingBody>) -> Option<String> {
         _ => None,
     }
 }
-
-// on any fail return nothing
-// fn get_ranges_2(range_str: &str, size: usize) -> Option<Vec<(usize, usize)>> {
-//     let stripped_range = range_str.trim();
-//     let range_values_str = match stripped_range.strip_prefix("bytes=") {
-//         Some(r) => r,
-//         _ => return None,
-//     };
-
-//     // track the last
-//     let mut last_last = 0;
-
-//     let mut ranges: Vec<(usize, usize)> = Vec::new();
-//     for value_str in range_values_str.split(",") {
-//         let trimmed_value_str = value_str.trim();
-
-//         // Range: <unit>=-<suffix-length>
-//         //
-//         // M is the byte size of file
-//         // N is the start index
-//         // possible seek and read from N -> M
-//         //
-//         if let Some(without_suffix) = trimmed_value_str.strip_suffix("-") {
-//             // parse int
-//             // push
-//             let start_range_int: usize = match without_suffix.parse() {
-//                 Ok(sri) => sri,
-//                 _ => return None,
-//             };
-
-//             // overlap
-//             if start_range_int < last_last {
-//                 return None;
-//             }
-//             last_last = size;
-
-//             ranges.push((start_range_int, size));
-//             continue;
-//         }
-
-//         // Range: <unit>=<range-start>-
-//         //
-//         // M is byte size of file
-//         // N is the
-//         // possible seek and read from (M - N) -> M
-//         //
-//         if let Some(without_prefix) = trimmed_value_str.strip_prefix("-") {
-//             // possible suffix
-//             let end_range_int: usize = match without_prefix.parse() {
-//                 Ok(sri) => sri,
-//                 _ => return None,
-//             };
-
-//             if end_range_int >= size {
-//                 return None;
-//             }
-
-//             let start_range_int = size - end_range_int;
-
-//             // overlap
-//             if start_range_int < last_last {
-//                 return None;
-//             }
-//             last_last = size;
-
-//             ranges.push((start_range_int, size));
-//             continue;
-//         }
-
-//         // Range: <unit>=<range-start>-<range-end>
-//         //
-//         let mut values = trimmed_value_str.split("-");
-
-//         let start_range_str = match values.next() {
-//             Some(start_range) => start_range,
-//             _ => return None,
-//         };
-
-//         let end_range_str = match values.next() {
-//             Some(end_range) => end_range,
-//             _ => return None,
-//         };
-
-//         let start_range_int: usize = match start_range_str.parse() {
-//             Ok(sri) => sri,
-//             _ => return None,
-//         };
-
-//         let end_range_int: usize = match end_range_str.parse() {
-//             Ok(sri) => sri,
-//             _ => return None,
-//         };
-
-//         // check bounds
-//         // overlap
-//         if start_range_int < last_last {
-//             return None;
-//         }
-//         last_last = end_range_int;
-
-//         if start_range_int > end_range_int {
-//             return None;
-//         }
-
-//         // swap for "size", never read more than size
-//         if end_range_int > size {
-//             return None;
-//         }
-
-//         ranges.push((start_range_int, end_range_int))
-//     }
-
-//     return Some(ranges);
-// }
 
 // on any fail return nothing
 fn get_ranges(range_str: &str) -> Option<Vec<(Option<usize>, Option<usize>)>> {
@@ -256,7 +125,7 @@ fn get_ranges(range_str: &str) -> Option<Vec<(Option<usize>, Option<usize>)>> {
                 _ => return None,
             };
 
-            ranges.push((Some(start_range_int), None));
+            ranges.push((None, Some(start_range_int)));
             continue;
         }
 
@@ -273,7 +142,7 @@ fn get_ranges(range_str: &str) -> Option<Vec<(Option<usize>, Option<usize>)>> {
                 _ => return None,
             };
 
-            ranges.push((None, Some(end_range_int)));
+            ranges.push((Some(end_range_int), None));
             continue;
         }
 
@@ -314,26 +183,89 @@ fn build_content_range_header_str(start: &usize, end: &usize, size: &usize) -> S
     "bytes ".to_string() + &start.to_string() + "-" + &end.to_string() + "/" + &size.to_string()
 }
 
-async fn build_single_range_response(
-    mut file_to_read: File,
-    metadata: Metadata,
-    content_encoding: Option<String>,
-    ranges: Vec<(usize, usize)>,
+async fn build_range_responses(
+    filepath: &PathBuf,
     content_type: &str,
+    status_code: StatusCode,
+    encodings: &Option<Vec<String>>,
+    ranges: Vec<(Option<usize>, Option<usize>)>,
 ) -> Option<Result<BoxedResponse, hyper::http::Error>> {
-    // build response
-    let size = metadata.len() as usize;
+    if let Some(res) =
+        compose_enc_range_response(&filepath, content_type, status_code, &encodings, &ranges).await
+    {
+        return Some(res);
+    };
 
-    let (start, end) = match ranges.get(0) {
-        Some(f) => f,
+    // origin target
+    if let Some(res) =
+        compose_single_range_response(&filepath, content_type, status_code, None, &ranges).await
+    {
+        return Some(res);
+    }
+
+    None
+}
+
+async fn compose_enc_range_response(
+    filepath: &PathBuf,
+    content_type: &str,
+    status_code: StatusCode,
+    encodings: &Option<Vec<String>>,
+    ranges: &Vec<(Option<usize>, Option<usize>)>,
+) -> Option<Result<BoxedResponse, hyper::http::Error>> {
+    let encds = match encodings {
+        Some(encds) => encds,
         _ => return None,
     };
 
-    let _cursor = match file_to_read
-        // this could be problematic for other systems that are not u64
-        .seek(SeekFrom::Start(start.clone() as u64))
-        .await
-    {
+    for enc in encds {
+        if let Some(encoded_path) = add_extension(filepath, &enc) {
+            if let Some(res) = compose_single_range_response(
+                &encoded_path,
+                content_type,
+                status_code,
+                Some(enc),
+                ranges,
+            )
+            .await
+            {
+                return Some(res);
+            }
+        };
+    }
+
+    None
+}
+
+async fn compose_single_range_response(
+    filepath: &PathBuf,
+    content_type: &str,
+    status_code: StatusCode,
+    content_encoding: Option<&str>,
+    ranges: &Vec<(Option<usize>, Option<usize>)>,
+) -> Option<Result<BoxedResponse, hyper::http::Error>> {
+    // build response
+    let mut file = match File::open(filepath).await {
+        Ok(m) => m,
+        _ => return None,
+    };
+
+    let metadata = match file.metadata().await {
+        Ok(m) => m,
+        _ => return None,
+    };
+
+    let size = metadata.len() as usize;
+
+    // check if end is within byte range
+    let (start, end) = match ranges.get(0) {
+        Some((None, Some(end))) => (size - end, size),
+        Some((Some(start), None)) => (start.clone(), size),
+        Some((Some(start), Some(end))) => (start.clone(), end.clone()),
+        _ => return None,
+    };
+
+    let _cursor = match file.seek(SeekFrom::Start(start.clone() as u64)).await {
         Ok(crsr) => crsr,
         _ => return None,
     };
@@ -341,8 +273,8 @@ async fn build_single_range_response(
     let mut buffer: Vec<u8> = Vec::with_capacity(end - start + 1);
     buffer.resize(end - start + 1, 0);
 
-    let content_range_header = build_content_range_header_str(start, end, &size);
-    let reader_stream = ReaderStream::with_capacity(file_to_read, size);
+    let content_range_header = build_content_range_header_str(&start, &end, &size);
+    let reader_stream = ReaderStream::with_capacity(file, size);
     let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
     let boxed_body = stream_body.boxed();
 
@@ -358,96 +290,3 @@ async fn build_single_range_response(
 
     return Some(builder.body(boxed_body));
 }
-
-// Multi-part ranges are particularly difficult to stream frame by frame.
-// Solution could be a facade buffer that can:
-//   - read a file frame by frame
-//   - over multiple ranges
-//   - while including boundaries
-//
-// But the most cause for concern is the ability to load an entire file in memory.
-// That is not good.
-//
-// Below is a potential way of serving multpart files.
-// For now it seems not worth including.
-//
-//
-// async fn build_multipart_range_response(
-//     mut file_to_read: File,
-//     metadata: Metadata,
-//     content_encoding: Option<String>,
-//     ranges: Vec<(usize, usize)>,
-//     content_type: &str,
-// ) -> Option<Result<BoxedResponse, hyper::http::Error>> {
-//     let boundary_core = "k1ng_0f_1nf1n1t3_5pac3";
-//     let boundary = "--".to_string() + boundary_core;
-
-//     // create a byte array
-//     let mut big_buffer: Vec<u8> = Vec::new();
-
-//     for range in ranges {
-//         let (start, end) = range;
-
-//         let _cursor = match file_to_read
-//             // this could be problematic for other systems that are not u64
-//             .seek(SeekFrom::Start(start.clone() as u64))
-//             .await
-//         {
-//             Ok(crsr) => crsr,
-//             _ => return None,
-//         };
-
-//         let mut buffer: Vec<u8> = Vec::with_capacity(end - start + 1);
-//         buffer.resize(end - start + 1, 0);
-
-//         // read file
-//         if let Err(_e) = file_to_read.read_exact(&mut buffer).await {
-//             return None;
-//         };
-
-//         // then add boundary
-//         big_buffer.extend(boundary.as_bytes());
-//         big_buffer.extend("\n".as_bytes());
-
-//         // then add headers
-//         let multi_headers = CONTENT_TYPE.to_string()
-//             + ": "
-//             + content_type
-//             + "\n"
-//             // gotta add content encoding
-//             + CONTENT_RANGE.as_str()
-//             + ": bytes "
-//             + &start.to_string()
-//             + "-"
-//             + &end.to_string()
-//             + "/"
-//             + &metadata.len().to_string()
-//             + "\n\n";
-//         big_buffer.extend(multi_headers.as_bytes());
-//         big_buffer.extend(buffer);
-//         big_buffer.extend("\n".as_bytes());
-//     }
-
-//     // add final boundary
-//     big_buffer.extend(boundary.as_bytes());
-//     big_buffer.extend("--".as_bytes());
-
-//     // Now build a response
-//     let req_content_type = "multipart/byteranges; boundary=".to_string() + boundary_core;
-
-//     let mut builder = Response::builder()
-//         .status(StatusCode::PARTIAL_CONTENT)
-//         .header(CONTENT_TYPE, req_content_type);
-
-//     if let Some(enc) = content_encoding {
-//         builder = builder.header(CONTENT_ENCODING, enc);
-//     }
-
-//     return Some(
-//         builder.header(CONTENT_LENGTH, big_buffer.len()).body(
-//             Full::new(bytes::Bytes::from(big_buffer))
-//                 .map_err(|e| match e {})
-//                 .boxed(),
-//         ),
-//     );
-// }
